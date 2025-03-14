4533d5f2b48432b5d87e233c12bcd3e794b9d685
Martin <idonot@like.spam>
2025-03-13 20:35:40 -0500

    moved events model to

--- events/admin.py
+++ events/admin.py
@@ -1,18 +1,44 @@
 from django.contrib import admin
-
 from .models import Event
 
+
+@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('created', 'level', 'name', 'email', 'country', 'ip', 'ref')
-    list_filter = ('level', 'country', 'created')
-    search_fields = ('name', 'email', 'country', 'ip', 'ref')
-    readonly_fields = ('created',)
-    date_hierarchy = 'created'
-    ordering = ('-created',)
+    list_display = (
+        'id', 'level', 'name', 'email', 'country', 'ip', 'created',
+        'is_mobile', 'is_bot', 'os', 'device'
+    )
+
+    list_filter = (
+        'level', 'country', 'is_mobile', 'is_bot', 'os', 'created'
+    )
+
+    search_fields = (
+        'name', 'email', 'ip', 'country', 'user_agent', 'device', 'os'
+    )
+
+    list_editable = ('level', )
+
+    readonly_fields = [
+        field.name for field in Event._meta.fields if field.name != 'level'
+    ]
+
     fieldsets = (
-        (None, {
-            'fields': ('created', 'level', 'name', 'email', 'country', 'ip', 'body', 'ref')
+        ('Basic Information', {
+            'fields': ('level', 'name', 'email', 'country', 'created')
+        }),
+        ('Request', {
+            'fields': ('body', 'ip', 'ref', 'sid', 'url')
+        }),
+        ('Device Information', {
+            'fields': (
+                'user_agent', 'device', 'browser', 'browser_family',
+                'is_mobile', 'is_tablet', 'is_touch_capable', 'is_pc', 'is_bot',
+                'os', 'os_family', 'os_version',
+                'device_family', 'device_brand', 'device_model'
+            )
         }),
     )
 
-admin.site.register(Event, EventAdmin)
+    date_hierarchy = 'created'
+    list_per_page = 50
--- events/models.py
+++ events/models.py
@@ -1,14 +1,97 @@
-from django.db import models
+from django.db.models import *
+from django.contrib.auth.models import User
+from user_agents import parse
 
-class Event(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    level = models.CharField(max_length=256, null=False, default="INFO")
-    name = models.CharField(max_length=256, null=False)
-    email = models.CharField(max_length=256, null=False, default="")
-    country = models.CharField(max_length=256, null=False, default="")
-    ip = models.CharField(max_length=256, null=False, default="")
-    body = models.TextField(null=False, default="{}")
-    ref = models.CharField(max_length=256, null=False, default="")
+from utils import get_ip
+
+
+class Event(Model):
+    created = DateTimeField(auto_now_add=True)
+
+    LEVEL_CHOICES = (
+        (0, 'Debug'),
+        (1, 'Info'),
+        (2, 'Warning'),
+        (3, 'Error')
+    )
+    level = IntegerField(null=False, default=1, choices=LEVEL_CHOICES)
+    name = CharField(max_length=256, null=False)
+    email = CharField(max_length=256, null=False, default="")
+    country = CharField(max_length=256, null=False, default="")
+    ref = CharField(max_length=256, null=False, default="")
+    sid = CharField(max_length=256, null=False, default='')
+    url = CharField(max_length=256, null=False, default='')
+    val1 = CharField(max_length=256, null=False, default='')
+    val2 = CharField(max_length=256, null=False, default='')
+    val3 = CharField(max_length=256, null=False, default='')
+    ip = CharField(max_length=256, null=False, default="")
+    body = TextField(null=False, default="{}")
+    user_agent = CharField(max_length=1024, null=False, default='')
+    device = CharField(max_length=256, null=False, default='')
+    browser = CharField(max_length=256, null=False, default='')
+    browser_family = CharField(max_length=256, null=False, default='')
+    is_mobile = BooleanField(null=False, default=False)
+    is_tablet = BooleanField(null=False, default=False)
+    is_touch_capable = BooleanField(null=False, default=False)
+    is_pc = BooleanField(null=False, default=False)
+    is_bot = BooleanField(null=False, default=False)
+    os = CharField(max_length=256, null=False, default='')
+    os_family = CharField(max_length=256, null=False, default='')
+    os_version = CharField(max_length=256, null=False, default='')
+    device_family = CharField(max_length=256, null=False, default='')
+    device_brand = CharField(max_length=256, null=False, default='')
+    device_model = CharField(max_length=256, null=False, default='')
 
     def __str__(self):
         return f'{self.level} - {self.name} - {self.email}'
+
+    @classmethod
+    def debug(cls, **kwargs):
+        return cls._create_event(level=0, **kwargs)
+
+    @classmethod
+    def info(cls, **kwargs):
+        return cls._create_event(level=1, **kwargs)
+
+    @classmethod
+    def warn(cls, **kwargs):
+        return cls._create_event(level=2, **kwargs)
+
+    @classmethod
+    def err(cls, **kwargs):
+        return cls._create_event(level=3, **kwargs)
+
+    @classmethod
+    def _create_event(cls, level, request=None, **kwargs):
+        def ip2country(ip):
+            return 'TODO'
+
+        user = getattr(request, 'user', None)
+        if user and isinstance(user, User):
+            kwargs.setdefault('email', user.email)
+
+        ip = ""
+        country = ""
+        if request:
+            kwargs.setdefault('ip', get_ip(request))
+            kwargs.setdefault('country', ip2country(kwargs['ip']))
+
+            user_agent = request.headers.get("User-Agent", "")
+            ua = parse(user_agent)
+            kwargs.setdefault('user_agent', user_agent)
+            kwargs.setdefault('device', ua.device.family)
+            kwargs.setdefault('browser', ua.browser.family)
+            kwargs.setdefault('browser_family', ua.browser.family)
+            kwargs.setdefault('is_mobile', ua.is_mobile)
+            kwargs.setdefault('is_tablet', ua.is_tablet)
+            kwargs.setdefault('is_touch_capable', ua.is_touch_capable)
+            kwargs.setdefault('is_pc', ua.is_pc)
+            kwargs.setdefault('is_bot', ua.is_bot)
+            kwargs.setdefault('os', ua.os.family)
+            kwargs.setdefault('os_family', ua.os.family or '')
+            kwargs.setdefault('os_version', ua.os.version_string or '')
+            kwargs.setdefault('device_family', ua.device.family or '')
+            kwargs.setdefault('device_brand', ua.device.brand or '')
+            kwargs.setdefault('device_model', ua.device.model or '')
+
+        return cls.objects.create(level=level, **kwargs)
