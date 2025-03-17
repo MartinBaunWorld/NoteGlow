86f9c09a24b93b786055580823e3dd306700379a
Martin <idonot@like.spam>
2025-03-17 17:12:47 -0500

    markdown improvements

--- utils/markdown.py
+++ utils/markdown.py
@@ -1,9 +1,8 @@
 from markdown import markdown as _markdown
-from markdown.extensions.fenced_code import FencedCodeExtension as _FencedCodeExtension
 
 
 def markdown(txt):
     return _markdown(
         txt,
-        extensions=[_FencedCodeExtension()]
+        extensions=['fenced_code', 'sane_lists']
     )
